9774194567c4edaffcf33d31bfa542acc31cdedf
harun tharik <100503485+haruntharik@users.noreply.github.com>
2022-03-14 16:13:43 +0530

    Add files via upload
    After adding the request method for login page

--- stem/backen.py
+++ stem/backen.py
@@ -1,9 +1,14 @@
 from flask import Flask,redirect,render_template,request,session,abort
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/',)
 def my_form():
     return render_template('login.html')
+@app.route('/login',methods =['POST','GET'])
+def login():
+    if request.method =='POST' and request.form['uname']=='admin' and request.form['pass']=='admin123':
+        return  render_template('page2.html')
+
 @app.route('/route/')
 def route1():
     return render_template('page2.html')
