bab6ae8c8c1f0ab773af88b91d39112d46953e5c
harun tharik <100503485+haruntharik@users.noreply.github.com>
2022-03-14 08:12:22 +0530

    Add files via upload
    On day 13/03/2022

--- backen.py
+++ backen.py
@@ -3,5 +3,9 @@ from flask import Flask,redirect,render_template,request,session,abort
 app = Flask(__name__)
 @app.route('/')
 def my_form():
-    return render_template('login.html')
+    return render_template('page3.html')
+@app.route('/route/')
+def route1():
+    return render_template('page2.html')
+
 app.run()
