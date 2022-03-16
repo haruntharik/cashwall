1bbd44547e024da0b1fa6638576aafc65019cd12
harun tharik <100503485+haruntharik@users.noreply.github.com>
2022-03-16 09:34:09 +0530

    Add files via upload
    After the completion of login

--- stem/backen.py
+++ stem/backen.py
@@ -1,4 +1,7 @@
 from flask import Flask,redirect,render_template,request,session,abort
+import mysql.connector
+db=mysql.connector.connect(host="localhost",user="root",passwd="///Tharik321",database="wallet")
+cursor =db.cursor()
 
 app = Flask(__name__)
 @app.route('/',)
@@ -6,8 +9,15 @@ def my_form():
     return render_template('login.html')
 @app.route('/login',methods =['POST','GET'])
 def login():
-    if request.method =='POST' and request.form['uname']=='admin' and request.form['pass']=='admin123':
-        return  render_template('page2.html')
+    username=request.form['uname']
+    password=request.form['pass']
+    cursor.execute("SELECT * FROM users WHERE name='" + username +"' AND password=" + password+ "")
+    data = cursor.fetchone()
+    if data is None:
+
+        return  render_template('login.html')
+    else:
+        return render_template('page2.html')
 @app.route('/start',methods = ['POST','GET'])
 def start():
     if request.method =='POST':
@@ -22,5 +32,22 @@ def route2():
 @app.route('/route3/')
 def route3():
     return render_template(('table.html'))
+@app.route('/signup',methods =['POST','GET'])
+def signup():
+    if request.method=='POST':
+        details=request.form
+        name=details['usern']
+        password=details['passw']
+        walletid=details['wallid']
+        balance=details['balance']
+
+        cursor.execute("INSERT INTO users (name,password,walletid,balance) VALUES(%s,%s,%s,%s)",[name,password,walletid,balance])
+
+
+
+
+        db.commit()
+        return render_template('page2.html')
 
 app.run()
+cursor.close()
